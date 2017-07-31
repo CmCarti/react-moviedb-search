db1998761d45334fcbd2c91fbda2192a7f237ac7
Chad cartier <chadcartier@pcm.local>
2017-07-31 16:42:58 -0400

    Actor loop added

--- src/components/main-details.js
+++ src/components/main-details.js
@@ -2,12 +2,24 @@ import React from 'react';
 
 export default function MainDetails(props) {
     const film = props.film;
-    const cast = film.credits.cast.slice(0, 10);
-    console.log(cast);
+    const cast = film.credits.cast.slice(0, 5).map((actor) => {
+        return (
+            <div className="cast__member" key={ actor.name }>
+                <img src={`https://image.tmdb.org/t/p/w138_and_h175_bestv2/${actor.profile_path}`} alt={actor.name}/>
+                <h4 className="cast__name">{ actor.name }</h4>
+                <span className="cast__character">{ actor.character }</span>
+            </div>
+        );
+    });
+    
 
     return (
-        <div className="main-details">
-            
+        <div className="main-details container">
+            <div className="row">
+                <div className="col-sm-8">
+                    { cast }
+                </div>
+            </div>
         </div>
     );
 }
